89e65b10a9637ffa9820abf3d7b960fe9b6fff36
Bruno Buonassisa <brunobuona@forgethecode.com>
2022-12-08 23:56:27 -0300

    1.3.1

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Main from './src/screens/Main.jsx';
-import Login from './src/screens/Login';
+import Login from './src/screens/Login.jsx';
+import Register from './src/screens/Register.jsx';
+import Home from './src/screens/Home.jsx';
 const Stack = createNativeStackNavigator();
 export default function App() {
   return(
   <NavigationContainer>
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Main} />
+      <Stack.Screen name="MyTinerary" component={Main} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Home" component={Home} />
     </Stack.Navigator>
   </NavigationContainer>
 )
